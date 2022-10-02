8a18838f8bdddf388ae8ac02b9ad2025d6fe278b
opariffazman <opariffazman@gmail.com>
2022-10-02 19:25:13 +0800

    add app.listen

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
+const app = express()
+
 const processSomething = callback => {
   setTimeout(callback, 20000)
 }
@@ -9,10 +11,12 @@ router.post("/hook", (req, res, next) => {
   processSomething(() => {
     const webhookUrl = req.params.url
 
-    console.log(`webhookUrl called`)
-  });
+    console.log(`${webhookUrl} called`)
+  })
 
   res.status(200).send('OK')
 })
 
 module.exports = router
+
+app.listen(port, () => console.log(`Telegram ShopBot listening at ${port}`))
