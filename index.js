c7f0c22b1e1e785140a3e4577167cac9b3fbf0b8
opariffazman <opariffazman@gmail.com>
2022-10-02 16:24:56 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const TelegramBot = require('node-telegram-bot-api')
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+const BOT_TOKEN = process.env.BOT_TOKEN | "5421681843:AAGeQcrYjBU4Qa-GdDeMgh9tjKu3RRCucJo"
+const PAYMENT_TOKEN = process.env.PAYMENT_TOKEN | "284685063:TEST:NjQ1ZjMxNTEwYmRj"
+
+const telegramBot = new TelegramBot(BOT_TOKEN, { polling: true })
+
+telegramBot.on('error', msg => console.log(`[bot] error`, msg))
+telegramBot.on('polling_error', msg => console.log(`[bot] polling_error`, msg))
+telegramBot.on('webhook_error', msg => console.log(`[bot] webhook_error`, msg))
+
+telegramBot.setMyCommands([{
+  command: 'list',
+  description: 'show products'
+}])
+
+telegramBot.onText(/\/list/, async (msg, match) => {
+  telegramBot.sendMessage(msg.chat.id, 'Select a product', {
+    reply_markup: {
+      inline_keyboard: [
+        'test'
+      ]
+    }
+  })
+})
