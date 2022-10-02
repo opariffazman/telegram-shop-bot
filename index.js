4addd400f09db880f5ac1f9375ce7abc4af2b1b0
opariffazman <opariffazman@gmail.com>
2022-10-02 18:59:48 +0800

    remove unneeded stuffs, start from scratch

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
-const express = require('express')
-const expressApp = express()
-const axios = require("axios");
-const path = require("path")
-const port = process.env.PORT || 3000;
-expressApp.use(express.static('static'))
-expressApp.use(express.json());
-require('dotenv').config();
-
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
 
-expressApp.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-bot.command('start', ctx => {
-  console.log(ctx.from)
-  bot.telegram.sendMessage(ctx.chat.id, 'Hello World', {
-  })
-})
-
-bot.command('ethereum', ctx => {
-  var rate;
-  console.log(ctx.from)
-  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
-  .then(response => {
-    console.log(response.data)
-    rate = response.data.ethereum
-    const message = `Hello, today the ethereum price is ${rate.usd}USD`
-    bot.telegram.sendMessage(ctx.chat.id, message, {
-    })
-  })
-})
-
-bot.launch()
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
