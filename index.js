22e8885db80b760ef1b0bfd297a89e6003c46fa6
opariffazman <opariffazman@gmail.com>
2022-10-02 16:39:05 +0800

    replaced old telegram bot api with telegraf

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 const express = require('express')
-const TelegramBot = require('node-telegram-bot-api')
+const expressApp = express()
+const axios = require("axios");
+const path = require("path")
+const port = process.env.PORT || 3000;
+expressApp.use(express.static('static'))
+expressApp.use(express.json());
+require('dotenv').config();
 
-const port = process.env.PORT || 3000
+const { Telegraf } = require('telegraf');
 
-const app = express()
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const BOT_TOKEN = process.env.BOT_TOKEN
-const PAYMENT_TOKEN = process.env.PAYMENT_TOKEN
+expressApp.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-const telegramBot = new TelegramBot(BOT_TOKEN, { polling: true })
-
-telegramBot.on('error', msg => console.log(`[bot] error`, msg))
-telegramBot.on('polling_error', msg => console.log(`[bot] polling_error`, msg))
-telegramBot.on('webhook_error', msg => console.log(`[bot] webhook_error`, msg))
-
-telegramBot.setMyCommands([{
-  command: 'list',
-  description: 'show products'
-}])
-
-telegramBot.onText(/\/list/, async (msg, match) => {
-  telegramBot.sendMessage(msg.chat.id, 'Select a product', {
-    reply_markup: {
-      inline_keyboard: [
-        'test'
-      ]
-    }
-  })
-})
-
-
-app.listen(port, () => console.log(`Telegram ShopBot listening at ${port}`))
+bot.launch()
