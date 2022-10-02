a6bb9c8bcd9c5e67411bbef4068f4a9c60aff26d
opariffazman <opariffazman@gmail.com>
2022-10-02 19:56:34 +0800

    implement telegram bot and hook launch method

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require( 'express' );
-const app = express();
-app.use( express.json() );
+import { Telegraf } from "telegraf";
+const express = require('express')
 
-app.post( '/', ( req, res ) => {
-    console.log( 'received webhook', req.body );
-    res.sendStatus( 200 );
-} );
+// webhook
+const app = express()
+app.use(express.json())
 
-app.listen( 9000, () => console.log( 'Node.js server started on port 9000.' ) );
+app.post('/', (req, res) => {
+  console.log('received webhook', req.body)
+  res.sendStatus(200)
+})
+
+const port = process.env.PORT || 9000
+app.listen(port, () => console.log(`Node.js server started on port ${port}.`))
+
+// telegram bot
+const token = process.env.BOT_TOKEN
+const bot = new Telegraf(token)
+
+bot.on("text", ctx => ctx.reply("Hello"))
+
+// Start webhook via launch method (preferred)
+bot.launch({
+  webhook: {
+    // Public domain for webhook; e.g.: example.com
+    domain: 'https://prickly-coat-mite.cyclic.app/',
+
+    // Port to listen on; e.g.: 8080
+    port: port,
+
+  },
+})
